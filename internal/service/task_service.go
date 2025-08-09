5f690a6eb47e86cd5aeef48a5e76ae0359e108d2
zhaoye <anti_utopia@163.com>
2025-08-09 21:24:01 +0800

    add db operation at POST, GET

--- internal/service/task_service.go
+++ internal/service/task_service.go
@@ -2,8 +2,13 @@ package service
 
 import (
 	"fmt"
+	"net/http"
+	"time"
 
+	"github.com/WangZhaoye/go-task-processor/internal/db"
+	"github.com/WangZhaoye/go-task-processor/internal/model"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 type TaskRequest struct {
@@ -12,10 +17,45 @@ type TaskRequest struct {
 }
 
 func CreateTask(c *gin.Context) {
-	fmt.Println("create task")
-	c.JSON(200, gin.H{"status": "ok"}) 
+	var req TaskRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	task := model.Task{
+		ID: uuid.New(),
+		Type: req.Type,
+		Payload: req.Payload,
+		Status: model.StatusPending,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	//create in DB
+	if err := db.DB.Create(&task).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save task"})
+		return
+	}
+
+	//push to MQ
+
+	fmt.Println("create task in DB ✅")
+	c.JSON(http.StatusCreated, task) 
 }
 
 func GetTask(c *gin.Context) {
-	fmt.Println("get task")
+	id := c.Param("id")
+	uuidVal, err := uuid.Parse(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error":  "Task not found"})
+		return
+	}
+	var task model.Task
+	if err := db.DB.First(&task, "id = ?", uuidVal).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+		return
+	}
+	fmt.Println("get task ✅")
+	c.JSON(http.StatusOK, task)
 }
